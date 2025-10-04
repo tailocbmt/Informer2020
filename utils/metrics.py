a779629714fd9e39526e6ca8819604ed5f1ba1ec
chiisa <70880420+gianghoang-seta@users.noreply.github.com>
2025-10-05 00:52:55 +0800

    feat: update informer

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,27 +1,65 @@
 import numpy as np
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,
+    confusion_matrix, classification_report,
+    ConfusionMatrixDisplay
+)
+
+
+def get_direction_value(value: float) -> int:
+    if value > 0.0:
+        return 1
+    elif value < 0.0:
+        return -1
+    return 0
+
+
+def directional_accuracy(pred, true):
+    vectorized_func = np.vectorize(get_direction_value)
+
+    pred_value = vectorized_func(pred)
+    true_value = vectorized_func(true)
+
+    acc = accuracy_score(true_value, pred_value)
+    prec = precision_score(true_value, pred_value,
+                           average="macro", zero_division=0)
+    rec = recall_score(true_value, pred_value,
+                       average="macro", zero_division=0)
+    f1 = f1_score(true_value, pred_value,
+                  average="macro", zero_division=0)
+
+    return acc, prec, rec, f1
+
 
 def RSE(pred, true):
     return np.sqrt(np.sum((true-pred)**2)) / np.sqrt(np.sum((true-true.mean())**2))
 
+
 def CORR(pred, true):
-    u = ((true-true.mean(0))*(pred-pred.mean(0))).sum(0) 
+    u = ((true-true.mean(0))*(pred-pred.mean(0))).sum(0)
     d = np.sqrt(((true-true.mean(0))**2*(pred-pred.mean(0))**2).sum(0))
     return (u/d).mean(-1)
+
 
 def MAE(pred, true):
     return np.mean(np.abs(pred-true))
 
+
 def MSE(pred, true):
     return np.mean((pred-true)**2)
+
 
 def RMSE(pred, true):
     return np.sqrt(MSE(pred, true))
 
+
 def MAPE(pred, true):
     return np.mean(np.abs((pred - true) / true))
 
+
 def MSPE(pred, true):
     return np.mean(np.square((pred - true) / true))
+
 
 def metric(pred, true):
     mae = MAE(pred, true)
@@ -29,5 +67,6 @@ def metric(pred, true):
     rmse = RMSE(pred, true)
     mape = MAPE(pred, true)
     mspe = MSPE(pred, true)
-    
-    return mae,mse,rmse,mape,mspe
+    acc, prec, rec, f1 = directional_accuracy(pred, true)
+
+    return mae, mse, rmse, mape, mspe, acc, prec, rec, f1
